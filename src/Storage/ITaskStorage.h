6b2f3324c39eb09cfd749ece13e2190eb87daf3f
ksrcr7 <kasrabakhshaei25@gmail.com>
2026-04-12 17:38:02 +0330

    Implement storage interface

--- src/Storage/ITaskStorage.h
+++ src/Storage/ITaskStorage.h
@@ -1,8 +1,29 @@
-//
-// Created by ASUS on 4/12/2026.
-//
-
 #ifndef MINI_BACKEND_SYSTEM_ITASKSTORAGE_H
 #define MINI_BACKEND_SYSTEM_ITASKSTORAGE_H
+
+#include "../Model/Task.h"
+#include <vector>
+
+
+class ITaskStorage {
+
+public:
+    virtual void add(const backend::Task& task) = 0;
+    [[nodiscard]] virtual std::vector<backend::Task> getAll() const = 0;
+    virtual void update(const backend::Task& task) = 0;
+    virtual ~ITaskStorage() = default;
+
+};
+
+
+
+
+
+
+
+
+
+
+
 
 #endif //MINI_BACKEND_SYSTEM_ITASKSTORAGE_H
