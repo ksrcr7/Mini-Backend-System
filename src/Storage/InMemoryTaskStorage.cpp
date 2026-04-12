b506c6785ef3477439aa90ba517f5751c4c3a9a7
ksrcr7 <kasrabakhshaei25@gmail.com>
2026-04-12 19:34:03 +0330

    Implement InMemoryTaskStorage source

--- src/Storage/InMemoryTaskStorage.cpp
+++ src/Storage/InMemoryTaskStorage.cpp
@@ -1,5 +1,24 @@
-//
-// Created by ASUS on 4/12/2026.
-//
-
 #include "InMemoryTaskStorage.h"
+#include <algorithm>
+
+void backend::InMemoryTaskStorage::add(const backend::Task &task) {
+    tasks.emplace_back(task);
+}
+
+std::vector<backend::Task> backend::InMemoryTaskStorage::getAll() const {
+    return tasks;
+}
+
+void backend::InMemoryTaskStorage::update(const backend::Task &task) {
+    auto it = std::find_if(tasks.begin(),tasks.end(),
+                           [&](const Task& t){
+                               return t.id == task.id;
+    });
+
+    if(it != tasks.end()){
+        *it = task;
+    }
+
+
+
+}
