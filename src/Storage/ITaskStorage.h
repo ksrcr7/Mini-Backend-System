a29b529b4532a37f61334d9bf44171ac676372b5
ksrcr7 <kasrabakhshaei25@gmail.com>
2026-04-18 00:29:54 +0330

    add new method findbyId()

--- src/Storage/ITaskStorage.h
+++ src/Storage/ITaskStorage.h
@@ -11,6 +11,7 @@ public:
     virtual void add(const backend::Task& task) = 0;
     [[nodiscard]] virtual std::vector<backend::Task> getAll() const = 0;
     virtual void update(const backend::Task& task) = 0;
+    virtual backend::Task* findbyId(int id) = 0;
     virtual ~ITaskStorage() = default;
 
 };
