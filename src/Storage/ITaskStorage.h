29d7a93afab69dbcb98140bacab67ad5db229f45
ksrcr7 <kasrabakhshaei25@gmail.com>
2026-04-18 08:57:36 +0330

    add new method removebyId()

--- src/Storage/ITaskStorage.h
+++ src/Storage/ITaskStorage.h
@@ -12,6 +12,7 @@ public:
     [[nodiscard]] virtual std::vector<backend::Task> getAll() const = 0;
     virtual void update(const backend::Task& task) = 0;
     virtual backend::Task* findbyId(int id) = 0;
+    virtual bool removebyId(int id) = 0;
     virtual ~ITaskStorage() = default;
 
 };
